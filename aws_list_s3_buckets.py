ee6b398926c39c6f0c4a33e73b8e0240de16a573
Daniel T <daniel@linuxfreelancer.com>
2017-09-22 10:23:46 -0400

    exception handling

--- aws_list_s3_buckets.py
+++ aws_list_s3_buckets.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 ''' Recursively list all files in S3 under your account '''
-import boto.ec2
+try:
+    import boto.ec2
+except Exception as e:
+    raise Exception(e)
 s3conn = boto.connect_s3()
-
-print "All buckets - \n"
+print("All buckets - \n")
 for mybkt in s3conn:
     print mybkt.name
-
-print "\n\n"
+print("\n\n")
 # All keys in a bucket
 print "Recurse across all buckets - \n"
 for mybkt in s3conn:
-    print "Bucket name : {0}".format(mybkt.name)
+    print("Bucket name : {0}".format(mybkt.name))
     for item in mybkt.get_all_keys():
-        print item.name
+        print(item.name)
