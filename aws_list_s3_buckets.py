8840f6798e6455ad1285e9ac47d7b1dd8343e4ab
Daniel T <daniel@linuxfreelancer.com>
2018-04-11 08:20:28 -0400

    update for pep8 compliance

--- aws_list_s3_buckets.py
+++ aws_list_s3_buckets.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
-''' Recursively list all files in S3 under your account '''
-try:
-    import boto.ec2
-except Exception as e:
-    raise Exception(e)
-s3conn = boto.connect_s3()
-print("All buckets - \n")
-for mybkt in s3conn:
-    print mybkt.name
-print("\n\n")
-# All keys in a bucket
-print "Recurse across all buckets - \n"
-for mybkt in s3conn:
-    print("Bucket name : {0}".format(mybkt.name))
-    for item in mybkt.get_all_keys():
-        print(item.name)
+import sys
+
+def main():
+    """ Recursively list all files in S3 under your account """
+    try:
+        import boto.ec2
+    except ImportError as e:
+        print("Install boto python module.")
+        raise Exception(e)
+    except Exception as e:
+        raise Exception(e)
+    s3conn = boto.connect_s3()
+    print("All buckets - \n")
+    for mybkt in s3conn:
+        print mybkt.name
+    print("\n\n")
+    # All keys in a bucket
+    print "Recurse across all buckets - \n"
+    for mybkt in s3conn:
+        print("Bucket name : {0}".format(mybkt.name))
+        for item in mybkt.get_all_keys():
+            print(item.name)
+
+if __name__ == "__main__":
+    sys.exit(main())
